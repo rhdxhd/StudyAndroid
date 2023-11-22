0af6bd767ea5124121cc4b699450d7f7267a4913
rhdxhd <144658731+rhdxhd@users.noreply.github.com>
2023-11-22 14:05:35 +0900

    .

--- And08_Activity_Intent/app/src/main/java/com/example/and08_activity_intent/MainActivity.java
+++ And08_Activity_Intent/app/src/main/java/com/example/and08_activity_intent/MainActivity.java
@@ -2,27 +2,84 @@ package com.example.and08_activity_intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     //현재는 onCreate메소드에서 대부분의 코드를 처리해도 무방함.
     //추후에 여러 화면 또는 Spring연동 시 새로고침의 등의 처리가 필요하다면 수명주기가 있다는 사실을 인지하고 사용하면 됨.
 
     //Spring(jsp..) 모든 개발 툴에서는 디버깅 모드가 제공이 됨.-> 이것을 활용하면 프로그램의 전체 구조를 빠르게 파악하거나 오류 수정이 수월함.
     private final String TAG = "액티비티 수명주기";
 
+
+    EditText edt1, edt2, edt3;
+    Button btn1, btn2, btn3;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: ");
+
+        Intent intent = getIntent(); //LoginActivity에서 보내준 인텐트를 객체로 초기화 시킴
+        Log.d(TAG, "onCreate: " + intent.getStringExtra("strkey")); //LoginActivity에서 넣어둔 이름을 그대로 씀 대소문자 구별있음.
+        Log.d(TAG, "onCreate: " + intent.getIntExtra("intkey", -1));
+        Log.d(TAG, "onCreate: " + intent.getIntExtra("intKey", -1));
+        //dto의 필드부분을 전부 바이트형태의 통신 하기 편한 방법으로 일자로 세움. "loginId" , "값"...
+        LoginDTO dto = (LoginDTO)  intent.getSerializableExtra("dtokey");
+        Log.d(TAG, "onCreate: " + dto.getLoginId() + " : " + dto.getLoginPw());
+
+        ArrayList<LoginDTO> list = (ArrayList<LoginDTO>) intent.getSerializableExtra("listkey");
+        Log.d(TAG, "onCreate: " +  list.size());
+
+
+
+
+
+
+
+
+
     }
 
+
+    //onStart에서 find등의 작업을 수행 예정. (setContentView라는 메소드가 실행 이후 ->onStart())
     @Override
     protected void onStart() {
         super.onStart();
         Log.d(TAG, "onStart: ");
+
+        edt1 = findViewById(R.id.edt1);
+        edt2 = findViewById(R.id.edt2);
+        edt3 = findViewById(R.id.edt3);
+
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+
+
+        //암시적 인텐트 : activity => action(내가 없는 기능)으로 사욯함.
+        edt1.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse( "tel:/" + edt1.getText().toString() ));
+            startActivity(intent);
+        });
+
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this); // implements View.OnClickListener
+
+
     }
     @Override
     protected void onResume() {
@@ -52,5 +109,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn2) {
+            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+            intent.putExtra(SearchManager.QUERY, edt2.getText().toString()); //putExtra("query", "검색어");
+            startActivity(intent);
+
+        }else if(v.getId() == R.id.btn3) {
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("smsto: " + Uri.encode("010-1234-5678")));
+            intent.putExtra("sms_body", edt3.getText().toString());
+            startActivity(intent);
+        }
     }
 }
