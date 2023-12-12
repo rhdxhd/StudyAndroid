5827ab525f399e26e92f0d0ae78eb8b1c112200e
rhdxhd <144658731+rhdxhd@users.noreply.github.com>
2023-12-12 10:00:20 +0900

    .

--- Project01_Kymtalk/app/src/main/java/com/example/project01_kymtalk/opentalk/OpenTalkSubFragment.java
+++ Project01_Kymtalk/app/src/main/java/com/example/project01_kymtalk/opentalk/OpenTalkSubFragment.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 
 import com.example.project01_kymtalk.R;
 import com.example.project01_kymtalk.databinding.FragmentOpenTalkSubBinding;
+import com.google.android.material.chip.Chip;
 
 public class OpenTalkSubFragment extends Fragment {
     FragmentOpenTalkSubBinding binding;
@@ -29,10 +30,29 @@ public class OpenTalkSubFragment extends Fragment {
         binding.recvOpensub3.setLayoutManager(new LinearLayoutManager(getContext()));
 
         binding.recvOpensub4.setAdapter(new OpenSubAdapter3(dao.getOpenSub4List()));
-        binding.recvOpensub4.setLayoutManager(new LinearLayoutManager(getContext() , LinearLayoutManager.HORIZONTAL , false));
+        binding.recvOpensub4.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+        //ViewGroup에 자식요소를 넣는것은 addView
+        //xml파일에서 사용이 가능한 모든 속성들은 메소드 형태로도 존재함.
+        //Context가 필요함(화면의 상태 정보를 꼭 알아야 하는 경우)
 
 
+        //binding.chipGrpOpensub.addView(chip);
+            String[] chipTexts = {"등산", "니트", "이영지", "김태형", "명품정보", "김남준", "밀덕", "시고르자브종", "닌텐도", "정국", "보드게임", "해외축구"};
+            for(int i=0; i<chipTexts.length; i++){
+                Chip chip = getChip(chipTexts[i]);
+                binding.chipGrpOpensub.addView(chip);
+            }
 
         return binding.getRoot();
+
     }
-}
+        Chip getChip(String text){
+            Chip chip = new Chip(getContext(), null, com.google.android.material.R.style.Widget_MaterialComponents_Chip_Choice);
+            chip.setText(text);
+            chip.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+            return chip;
+
+        }
+    }
