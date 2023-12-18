2aaac6a27e4ab1fd5eb4600c9f3e30500262e5ee
rhdxhd <144658731+rhdxhd@users.noreply.github.com>
2023-12-18 11:12:38 +0900

    .

--- Project02_Last/app/src/main/java/com/example/project02_last/MainActivity.java
+++ Project02_Last/app/src/main/java/com/example/project02_last/MainActivity.java
@@ -1,40 +1,45 @@
 package com.example.project02_last;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.example.project02_last.common.CommonConn;
+import com.example.project02_last.common.CommonService;
 import com.example.project02_last.customer.CustomerFragment;
 import com.example.project02_last.databinding.ActivityMainBinding;
+import com.example.project02_last.file.FileActivity;
 
-import me.ibrahimsn.lib.OnItemReselectedListener;
+import java.util.HashMap;
+
 import me.ibrahimsn.lib.OnItemSelectedListener;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements OnItemSelectedListener{
 
-
     ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         binding.bottomBar.setOnItemSelectedListener(this);
         onItemSelect(0);
     }
-
-            @Override
-            public boolean onItemSelect(int i) {
-                Log.d("btm", "onItemSelect: " + i);
-                if(i==0) {
-                    getSupportFragmentManager().beginTransaction().replace(binding.container.getId(), new CustomerFragment()).commit();
-
-
-            }
-                return true;
-            }
+    @Override
+    public boolean onItemSelect(int i) {
+        Log.d("btm", "onItemSelect: " +i);
+        if(i==0){
+            getSupportFragmentManager().beginTransaction().replace(binding.container.getId(),new CustomerFragment()).commit();
+        }else if(i==1){
+            Intent intent = new Intent(MainActivity.this , FileActivity.class);
+            startActivity(intent);
         }
+        return true;
+    }
+}
